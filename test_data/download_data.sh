bc292e2f3a7315034f7ea897cad3da94eed2778a
Andrew Rodriguez <andrew.rodriguez@txstate.edu>
2024-04-13 16:33:59 -0500

    Add download script for large inputs because github complains

---
+++ test_data/download_data.sh
@@ -0,0 +1,4 @@
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/refMrna.fa.gz -O human_mrna.fa.gz
+gunzip human_mrna.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/otoGar3/bigZips/otoGar3.fa.masked.gz -O bushbaby.fa.gz
+gunzip bushbaby.fa.gz
