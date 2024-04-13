12594d5d284c3331bc4ba8f30b554872d35b4fb4
Andrew Rodriguez <andrew.rodriguez@txstate.edu>
2024-04-13 17:45:51 -0500

    Add batch script

--- batch_submission.sh
+++ batch_submission.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1                # total number of nodes requested
 #SBATCH -n 1               # total number of tasks requested
 #SBATCH -p gpu-a100-dev              # queue name normal or development
-#SBATCH -t 00:45:00         # expected maximum runtime (hh:mm:ss)
+#SBATCH -t 01:00:00         # expected maximum runtime (hh:mm:ss)
 #SBATCH --mail-user=api10@txstate.edu
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
@@ -15,7 +15,7 @@ date
 # vars
 #
 
-RUNS=3
+RUNS=1
 
 #
 # speedup exp and scalability exp
@@ -25,6 +25,7 @@ export CUDA_VISIBLE_DEVICES=0
 echo "baseline"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
+	time ./bin/single_gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
 done
 echo "~~~"
 
@@ -32,6 +33,7 @@ export CUDA_VISIBLE_DEVICES=0
 echo "multi-gpu: 1 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
+	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
 done
 echo "~~~"
 
@@ -39,6 +41,7 @@ export CUDA_VISIBLE_DEVICES=0,1
 echo "multi-gpu: 2 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
+	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
 done
 echo "~~~"
 
@@ -46,6 +49,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 echo "multi-gpu: 3 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
+	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
 done
 echo "~~~"
 
