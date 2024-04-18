c216982aa70de148bb15a75d433fea705f360928
Andrew Rodriguez <andrew.rodriguez@txstate.edu>
2024-04-17 19:40:50 -0500

    Add results

--- batch_submission.sh
+++ batch_submission.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1                # total number of nodes requested
 #SBATCH -n 1               # total number of tasks requested
 #SBATCH -p gpu-a100              # queue name normal or development
-#SBATCH -t 03:45:00         # expected maximum runtime (hh:mm:ss)
+#SBATCH -t 01:35:00         # expected maximum runtime (hh:mm:ss)
 #SBATCH --mail-user=api10@txstate.edu
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
@@ -15,7 +15,7 @@ date
 # vars
 #
 
-RUNS=3
+RUNS=1
 
 #
 # speedup exp and scalability exp
@@ -26,6 +26,7 @@ echo "baseline"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
 	time ./bin/single_gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
+	mv human_mrna.fa-bushbaby.fa.csv "results/baseline-human_mrna.fa-bushbaby.fa.csv"
 done
 echo "~~~"
 
@@ -34,6 +35,7 @@ echo "multi-gpu: 1 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
 	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
+	mv human_mrna.fa-bushbaby.fa.csv "results/1GPU-human_mrna.fa-bushbaby.fa.csv"
 done
 echo "~~~"
 
@@ -42,6 +44,7 @@ echo "multi-gpu: 2 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
 	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
+	mv human_mrna.fa-bushbaby.fa.csv "results/2GPU-human_mrna.fa-bushbaby.fa.csv"
 done
 echo "~~~"
 
@@ -50,6 +53,7 @@ echo "multi-gpu: 3 GPU"
 for (( i = 1; i <= $RUNS; i += 1 ))
 do
 	time ./bin/gpu_cuda_workflow -query test_data/human_mrna.fa -ref test_data/bushbaby.fa -factor 0.1 > /dev/null
+	mv human_mrna.fa-bushbaby.fa.csv "results/3GPU-human_mrna.fa-bushbaby.fa.csv"
 done
 echo "~~~"
 
