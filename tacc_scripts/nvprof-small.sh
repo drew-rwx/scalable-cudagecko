d62d75ceba45b06a713540b67e9e0c6deeac53ec
Brandon Alexander Burtchell <bab334@txstate.edu>
2024-04-30 16:35:50 -0500

    Finally get runtime proportions (on zurich, denver didn't have enough main mem)

--- tacc_scripts/nvprof-small.sh
+++ tacc_scripts/nvprof-small.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CUDA_BASELINE=0
-CUDA_1_GPU=0
-CUDA_2_GPU=0,1
+CUDA_BASELINE=1
+CUDA_1_GPU=1
+CUDA_2_GPU=1,2
 
 BASELINE_BINARY=../bin/single_gpu_cuda_workflow
 OUR_BINARY=../bin/gpu_cuda_workflow
@@ -11,21 +11,20 @@ QUERY=myco.hyop.232.fasta
 REF=zebrafish.fa
 QUERY_PATH=../test_data/$QUERY
 REF_PATH=../test_data/$REF
-#NSYS_PREPEND='nvprof --devices all -f'  # generates a report and sqlite
-NSYS_PREPEND='nsys nvprof'  # generates a report and sqlite
+NSYS_PREPEND='nvprof --devices all -f'  # generates a report and sqlite
+#NSYS_PREPEND='nsys nvprof'  # generates a report and sqlite
 
-#echo "~baseline~"
-#export CUDA_VISIBLE_DEVICES=$CUDA_BASELINE
-#$NSYS_PREPEND -o ../results/small-baseline.nvprof $BASELINE_BINARY -query $QUERY_PATH -ref $REF_PATH
-#mv $QUERY-$REF.csv ../results/small-baseline.nvprof.csv
+echo "~baseline~"
+export CUDA_VISIBLE_DEVICES=$CUDA_BASELINE
+$NSYS_PREPEND -o ../results/small-baseline.nvprof $BASELINE_BINARY -query $QUERY_PATH -ref $REF_PATH
+mv $QUERY-$REF.csv ../results/small-baseline.nvprof.csv
 
-#echo "~1 GPU~"
-#export CUDA_VISIBLE_DEVICES=$CUDA_1_GPU
-#$NSYS_PREPEND -o ../results/small-1gpu.nvprof $OUR_BINARY -query $QUERY_PATH -ref $REF_PATH
-#mv "$QUERY-$REF.csv" ../results/small-1gpu.nvprof.csv
+echo "~1 GPU~"
+export CUDA_VISIBLE_DEVICES=$CUDA_1_GPU
+$NSYS_PREPEND -o ../results/small-1gpu.nvprof $OUR_BINARY -query $QUERY_PATH -ref $REF_PATH
+mv "$QUERY-$REF.csv" ../results/small-1gpu.nvprof.csv
 
 echo "~2 GPU~"
 export CUDA_VISIBLE_DEVICES=$CUDA_2_GPU
-#$NSYS_PREPEND -o ../results/small-2gpu.nvprof $OUR_BINARY -query $QUERY_PATH -ref $REF_PATH
-$OUR_BINARY -query $QUERY_PATH -ref $REF_PATH
-#mv "$QUERY-$REF.csv" ../results/small-2gpu.nvprof.csv
+$NSYS_PREPEND -o ../results/small-2gpu.nvprof $OUR_BINARY -query $QUERY_PATH -ref $REF_PATH
+mv "$QUERY-$REF.csv" ../results/small-2gpu.nvprof.csv
